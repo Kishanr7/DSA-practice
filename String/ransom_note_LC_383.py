a7dbbb616a22cf35ecb076c309ab7054b26a9899
Kishanr7 <kishanrekhadia79@gmail.com>
2024-11-23 14:27:25 +0530

    rasom note 2 methods done

--- String/ransom_note_LC_383.py
+++ String/ransom_note_LC_383.py
@@ -14,28 +14,7 @@ def can_construct(ransom, magazine):
     if count == len(ransom_dict):
         return True
     else:
-        return False
-    '''r_hash = create_hashmap(ransom)
-    m_hash = create_hashmap(magazine)
-    count=0
-    print(r_hash, m_hash)
-    for char,occ in r_hash.items():
-        if char in m_hash.keys() and occ <=m_hash[char]:
-            count+=1
-    if count == len(r_hash):
-        return True
-    else:
-        return False
-    
-def create_hashmap(s):
-    char_count={}
-    for char in s:
-        if char in char_count:
-            char_count[char] +=1
-        else:
-            char_count[char] = 1
-    return char_count'''
-            
+        return False        
     
 ransom = 'aa'
 magazine = 'aaan'
