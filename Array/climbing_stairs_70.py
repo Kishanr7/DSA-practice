da8a2f95652a8275a0d40fb4a521d4fd167bcfc7
Kishanr7 <kishanrekhadia79@gmail.com>
2025-08-26 15:00:32 +0530

    daily ques 26-08

---
+++ Array/climbing_stairs_70.py
@@ -0,0 +1,16 @@
+def climbStairs(n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+    return dp[n]
+
+print(climbStairs(5))  # Output: 8
+print(climbStairs(6))  # Output: 13
+print(climbStairs(7))  # Output: 21
+
---
+++ Array/coin_change_322.py
@@ -0,0 +1,16 @@
+def coinChange(coins, amount):
+    coins.sort()
+    dp = [0] * (amount + 1)
+    for i in range(1,amount+1):
+        minn = float('inf')
+        for coin in coins:
+            diff = i - coin
+            if diff < 0:
+                break
+            minn = min(minn, dp[diff] + 1)
+        dp[i] = minn
+    if dp[amount] < float('inf'):
+        return dp[amount]
+    else:
+        return -1
+print(coinChange([1,2,5], 11))  # Output: 20
