c5db9cdc7ccdf9cd1302c7f17fbea91d06087185
Kishanr7 <kishanrekhadia79@gmail.com>
2025-08-28 18:19:51 +0530

    Daily ques 28-08

---
+++ Array/longest_consecutive_subsequence_128.py
@@ -0,0 +1,19 @@
+def longestConsecutive(nums):
+    num_set = set(nums)
+    longest_streak = 0
+
+    for num in num_set:
+        if num - 1 not in num_set:
+            current_num = num
+            current_streak = 1
+
+            while current_num + 1 in num_set:
+                current_num += 1
+                current_streak += 1
+            
+            longest_streak = max(longest_streak, current_streak)
+    return longest_streak
+print(longestConsecutive([9,1,4,7,3,-1,0,5,8,-1,6]))
+print(longestConsecutive([100,4,200,1,3,2]))
+print(longestConsecutive([0,3,7,2,5,8,4,6,0,1]))
+print(longestConsecutive([1,0,1,2]))
