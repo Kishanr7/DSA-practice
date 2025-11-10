eab44e3e42b6a1161b240f0346d34974f3b1efe8
Kishanr7 <kishanrekhadia79@gmail.com>
2025-11-10 23:01:12 +0530

    last word length

--- String/Length_of_last_word_LC_58.py
+++ String/Length_of_last_word_LC_58.py
@@ -7,10 +7,21 @@
 # 4. Return the length of the last word.
 
 def length_of_last_word(s):
-    striped_array = s.strip().split(" ")
-    length_of_array = len(striped_array)
+    # striped_array = s.strip().split(" ")
+    # length_of_array = len(striped_array)
     
-    return len(striped_array[length_of_array-1])
+    # return len(striped_array[length_of_array-1])
+    # code without split and strip
+    i=len(s)-1
+    if len(s) == 1 and s[0] != ' ':
+        return 1
+    while s[i] == ' ':
+        i-=1
+    counter = 0
+    while s[i] != ' ' and i>=0:
+        i-=1
+        counter+=1
+    return counter
     
     
 s="fly me      to     the moon   "
