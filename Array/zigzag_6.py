40181938504538adc9433cdd21dd39dc6c6f9dc2
Kishanr7 <kishanrekhadia79@gmail.com>
2025-11-20 17:36:23 +0530

    zigzag conversion

---
+++ Array/zigzag_6.py
@@ -0,0 +1,25 @@
+def convert(s,numRows):
+    if numRows == 1:
+        return s
+    
+    i = 0
+    d = 1
+    
+    rows = [[] for _ in range(numRows)]
+    
+    for char in s:
+        rows[i].append(char)   
+        if i == 0:
+            d = 1
+        elif i == numRows - 1:
+            d = -1
+        i += d
+    
+    ret = ''
+    for i in range(numRows):
+        ret += ''.join(rows[i])
+    return ret
+
+s = "PAYPALISHIRING"
+numRows = 3
+print(convert(s, numRows))  # Output: "PAHNAPLSIIGYIR"
